6eb9e70ea94aa100da9b932bdc94767beee53d1d
joaooliveiraa <joaogetknow@gmail.com>
2021-09-08 10:54:31 -0300

    finish application

--- src/app/nova-transferencia/nova-trasnferencia.component.ts
+++ src/app/nova-transferencia/nova-trasnferencia.component.ts
@@ -2,6 +2,7 @@ import { TransferenciaService } from './../services/transferencia.service';
 import { Component, Output } from '@angular/core';
 import { EventEmitter } from '@angular/core';
 import { Transferencia } from '../models/transferencia.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -14,7 +15,7 @@ export class NovaTransferenciaComponent {
   valor: number;
   destino: number;
 
-  constructor(private service: TransferenciaService) {}
+  constructor(private service: TransferenciaService, private router: Router) {}
 
   transferir() {
     const valorEmitir: Transferencia = {
@@ -25,6 +26,7 @@ export class NovaTransferenciaComponent {
     this.service.adicionar(valorEmitir).subscribe((resultado) => {
       console.log(resultado);
       this.limparCampos();
+      this.router.navigateByUrl('extrato');
     },
     error => {
         console.error(error);
