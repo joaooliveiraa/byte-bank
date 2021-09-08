e01b800c99eee7757002ffa4b5b347cd30906906
joaooliveiraa <joaogetknow@gmail.com>
2021-09-08 10:27:54 -0300

    add and config app routing

---
+++ src/app/app-routing-module.ts
@@ -0,0 +1,18 @@
+import { NovaTransferenciaComponent } from './nova-transferencia/nova-trasnferencia.component';
+import { ExtratoComponent } from './extrato/extrato.component';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'extrato', pathMatch: 'full'}, //Para caso não achar nenhuma rota
+  { path: 'extrato', component: ExtratoComponent },
+  { path: 'nova-transferencia', component: NovaTransferenciaComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
